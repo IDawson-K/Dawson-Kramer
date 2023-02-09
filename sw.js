5432092137c358729e23e19c7cb3dcc7374c0297
IDawson-K <kramerdawson7@gmail.com>
2023-02-09 16:00:11 +0100

    SERVICE WORKER QUI REND FOU

--- sw.js
+++ sw.js
@@ -6,6 +6,10 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox
 
 const offlineFallbackPage = "offline.html";
 
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
+});
+
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
